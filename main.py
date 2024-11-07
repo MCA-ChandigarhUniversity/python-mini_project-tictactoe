09ea6ba5ac31dbcfa429295aff3fd7c0d76ce788
adbhut96 <100306370+adbhut96@users.noreply.github.com>
2024-11-07 20:13:18 +0530

    [FIX] - fixed the controller where borad is not making move

--- main.py
+++ main.py
@@ -8,25 +8,25 @@ def start_game():
     player_one = tk.Tk()
     player_two = tk.Tk()
 
-    # Initialize model
+    # Initializing model
     model = TicTacToeModel()
 
-    # Initialize views without the controllers
+    # Initializing views without the controllers
     view1 = TicTacToeView(player_one, "Player X")
     view2 = TicTacToeView(palyer_two, "Player O")
 
-    # Update callback functions for each player
+    # Updating callback functions for each player
     def update_from_player1(board, turn):
         controller2.update_view(board, turn)
 
     def update_from_player2(board, turn):
         controller1.update_view(board, turn)
 
-    # Initialize controllers with the model and views
+    # Initializing controllers with the model and views
     controller1 = TicTacToeController(model, view1, is_player1=True, update_callback=update_from_player1)
     controller2 = TicTacToeController(model, view2, is_player1=False, update_callback=update_from_player2)
 
-    # Pass the controllers to the views
+    # Passing the controllers to the views
     view1.set_controller(controller1)
     view2.set_controller(controller2)
 
